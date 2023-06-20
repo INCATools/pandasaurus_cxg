44115a1d153c7eef161125020424f144eb247ee8
Ugur Bayindir <ubyndr@gmail.com>
2023-06-20 15:18:17 +0100

    Added predicate logic to co_annotation_report

--- pandasaurus_cxg/anndata_analyzer.py
+++ pandasaurus_cxg/anndata_analyzer.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 import pandas as pd
 
 from pandasaurus_cxg.anndata_loader import AnndataLoader
@@ -12,16 +14,37 @@ class AnndataAnalyzer:
     def co_annotation_report(self):
         free_text_cell_type = [key for key, value in self._schema.items() if value]
         temp_result = []
-        result = []
         for text in free_text_cell_type:
-            if text in self._anndata_obs:
+            if text in self._anndata_obs.columns:
                 co_oc = (
                     self._anndata_obs[[text, "cell_type"]].drop_duplicates().reset_index(drop=True)
                 )
+                predicate_dict = co_oc.groupby("cell_type")[text].apply(list).to_dict()
+                co_oc["predicate"] = co_oc.apply(
+                    lambda row: Predicate.CLUSTER_MATCHES.value
+                    if row[text] in predicate_dict.get(row["cell_type"], [])
+                    and len(predicate_dict.get(row["cell_type"], [])) == 1
+                    else (
+                        Predicate.SUBCLUSTER_OF.value
+                        if row[text] in predicate_dict.get(row["cell_type"], [])
+                        else Predicate.CLUSTER_OVERLAPS.value
+                    ),  # All the other cases should be marked with 'cluster_overlaps', right?
+                    axis=1,
+                )
                 temp_result.extend(co_oc.to_dict(orient="records"))
+
         result = [
             [item for sublist in [[k, v] for k, v in record.items()] for item in sublist]
             for record in temp_result
         ]
 
-        return pd.DataFrame(result, columns=["field_name1", "value1", "field_name2", "value2"])
+        return pd.DataFrame(
+            [inner_list[:2] + inner_list[5:6] + inner_list[2:4] for inner_list in result],
+            columns=["field_name1", "value1", "predicate", "field_name2", "value2"],
+        )
+
+
+class Predicate(Enum):
+    CLUSTER_MATCHES = "cluster_matches"
+    CLUSTER_OVERLAPS = "cluster_overlaps"
+    SUBCLUSTER_OF = "subcluster_of"
