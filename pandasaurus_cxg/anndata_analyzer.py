ece99b139a4a650e6117a266fce1ca4244a991cf
Ismail Ugur Bayindir <ugur@ebi.ac.uk>
2023-07-07 12:38:42 +0100

    Co annotation analysis - enrichment extension [Merge after #16] (#22)
    * Added TODO
    
    * Added temporary schema and loader
    
    * Added temporary schema and loader
    
    * Added AnndataAnalyzer and co_annotation_report
    
    * Added predicate logic to co_annotation_report
    
    * Increased max-line-length
    
    * Updated schema.json
    
    * Updated pandasaurus version
    
    * Fixed the predicate logic and added supercluster_of predicate
    
    * Refactored the lambda operation
    
    * Removed duplicates from co_annotation_report
    
    * Updated pandasaurus version to 0.2.2
    
    * Updated pandasaurus version to 0.2.3
    
    * Added walkthrough.ipynb
    
    * Updated definition of enrichment methods
    
    * Added disease filtering in co_annotation_report and refactored the method
    
    * Refactored AnndataAnalyzer
    
    * Additional refactoring in AnndataAnalyzer
    
    * Refactored AnndataEnricher __init__() and added from_file_path method
    
    * Added enriched_co_annotation_report and refactored co_annotation_report

--- pandasaurus_cxg/anndata_analyzer.py
+++ pandasaurus_cxg/anndata_analyzer.py
@@ -1,9 +1,11 @@
 from enum import Enum
+import itertools
 import os
 from typing import List, Optional
 
 import pandas as pd
 
+from pandasaurus_cxg.anndata_enricher import AnndataEnricher
 from pandasaurus_cxg.anndata_loader import AnndataLoader
 from pandasaurus_cxg.schema.schema_loader import read_json_file
 
@@ -35,12 +37,74 @@ class AnndataAnalyzer:
             schema_path (str): The path to the schema file.
 
         """
+        self.file_path = file_path
         self._anndata = AnndataLoader.load_from_file(file_path)
         self._schema = read_json_file(schema_path)
 
-    def co_annotation_report(self, disease: Optional[str] = None):
+    def co_annotation_report(self, disease: Optional[str] = None, enrich: bool = False):
         """
         Generates a co-annotation report based on the provided schema.
+
+        Args:
+            disease (Optional[str]): A valid disease CURIE used to filter the rows based on the
+                given disease. If provided, only the rows matching the specified disease will be
+                included in the filtering process. Defaults to None if no disease filtering is
+                desired.
+            enrich: Flag to either enable or disable enrichment in co_annotation report.
+                Defaults to False.
+
+        Returns:
+            pd.DataFrame: The co-annotation report.
+
+        """
+        free_text_cell_type = [key for key, value in self._schema.items() if value]
+        temp_result = []
+        cell_type_combinations = list(itertools.combinations(free_text_cell_type, 2))
+        for combination in cell_type_combinations:
+            field_name_1 = combination[1]
+            field_name_2 = combination[0]
+            if (field_name_1 in self._anndata.obs.columns
+                and field_name_2 in self._anndata.obs.columns
+            ):
+                co_oc = self._filter_data_and_remove_duplicates(field_name_1, field_name_2, disease)
+
+                if enrich:
+                    co_oc = self._enrich_co_annotation(co_oc, field_name_1, field_name_2)
+
+                AnndataAnalyzer._assign_predicate_column(co_oc, field_name_1, field_name_2)
+                temp_result.extend(co_oc.to_dict(orient="records"))
+
+        result = [
+            [item for sublist in [[k, v] for k, v in record.items()] for item in sublist]
+            for record in temp_result
+        ]
+        unique_result = AnndataAnalyzer._remove_duplicates(result)
+        return pd.DataFrame(
+            [inner_list[:2] + inner_list[5:6] + inner_list[2:4] for inner_list in unique_result],
+            columns=["field_name1", "value1", "predicate", "field_name2", "value2"],
+        )
+
+    def _filter_data_and_remove_duplicates(self, field_name_1, field_name_2, disease):
+        # Filter the data based on the disease condition
+        co_oc = (
+            self._anndata.obs[
+                (self._anndata.obs["disease_ontology_term_id"].str.lower() == disease.lower())
+            ][[field_name_1, field_name_2]]
+            if disease
+            else self._anndata.obs[[field_name_1, field_name_2]]
+        )
+        # Drop duplicates
+        co_oc = co_oc.drop_duplicates().reset_index(drop=True)
+        return co_oc
+
+    def enriched_co_annotation_report(self, disease: Optional[str] = None):
+        """
+        Generates an enriched co-annotation report based on the provided schema. The enrichment
+        process will be performed by checking if any of the CL terms in the initial seed
+        (the set of CL terms used to initialize the Pandasaurus object) are also present in the
+        object column of the enrichment table. If a match is found, the co-annotation analysis
+        will be repeated, including everything that maps to this term, either directly or via
+        the enrichment table.
 
         Args:
             disease (Optional[str]): A valid disease CURIE used to filter the rows based on the
@@ -52,31 +116,16 @@ class AnndataAnalyzer:
             pd.DataFrame: The co-annotation report.
 
         """
-        free_text_cell_type = [key for key, value in self._schema.items() if value]
-        temp_result = []
-        # Iterate over field_name_2 and field_name_1 combinations
-        for field_name_2 in free_text_cell_type:
-            for field_name_1 in free_text_cell_type:
-                if (
-                    field_name_1 != field_name_2
-                    and field_name_1 in self._anndata.obs.columns
-                    and field_name_2 in self._anndata.obs.columns
-                ):
-                    co_oc = self._filter_data_and_remove_duplicates(
-                        field_name_1, field_name_2, disease
-                    )
-                    AnndataAnalyzer._assign_predicate_column(co_oc, field_name_1, field_name_2)
-                    temp_result.extend(co_oc.to_dict(orient="records"))
+        return self.co_annotation_report(self, disease, True)
 
-        result = [
-            [item for sublist in [[k, v] for k, v in record.items()] for item in sublist]
-            for record in temp_result
-        ]
-        unique_result = AnndataAnalyzer._remove_duplicates(result)
-        return pd.DataFrame(
-            [inner_list[:2] + inner_list[5:6] + inner_list[2:4] for inner_list in unique_result],
-            columns=["field_name1", "value1", "predicate", "field_name2", "value2"],
-        )
+    def _enrich_co_annotation(self, co_oc, field_name_1, field_name_2):
+        enricher = AnndataEnricher(self._anndata)
+        simple = enricher.simple_enrichment()
+        df = simple[simple["o"].isin(enricher.get_seed_list())][
+            ["s_label", "o_label"]
+        ].rename(columns={"s_label": field_name_1, "o_label": field_name_2})
+        co_oc = pd.concat([co_oc, df], axis=0).reset_index(drop=True)
+        return co_oc
 
     def _filter_data_and_remove_duplicates(self, field_name_1, field_name_2, disease):
         # Filter the data based on the disease condition
