74104823490126ccb13aa1f0ed9648a84d9cae94
Ugur Bayindir <ubyndr@gmail.com>
2023-07-14 14:03:15 +0100

    Added InvalidGraphFormat and MissingEnrichmentProcess exceptions

--- pandasaurus_cxg/utils/exceptions.py
+++ pandasaurus_cxg/utils/exceptions.py
@@ -9,3 +9,22 @@ class InvalidSlimName(Exception):
             f"{', '.join([slim.get('name') for slim in valid_slim_list])}."
         )
         super().__init__(self.message)
+
+
+class InvalidGraphFormat(Exception):
+    def __int__(self, _format: str, valid_formats: List[str]):
+        self.message = (
+            f"Graph format, {_format}, provided for save_rdf_graph is invalid. "
+            f"Please use one of {', '.join(valid_formats)}"
+        )
+        super().__init__(self.message)
+
+
+class MissingEnrichmentProcess(Exception):
+    def __init__(self, enrichment_methods: List[str]):
+        self.message = (
+            f"Any of the following enrichment methods from AnndataEnricher must be used before "
+            f"using enriched_rdf_graph method: "
+            f"{', '.join(enrichment_methods)}"
+        )
+        super().__init__(self.message)
