3ed6274cb47f62798093d4ac20991e987bdc7dd6
Ugur Bayindir <ubyndr@gmail.com>
2023-07-21 10:59:39 +0100

    Added CellTypeNotFoundError and SubclassWarning

--- pandasaurus_cxg/utils/exceptions.py
+++ pandasaurus_cxg/utils/exceptions.py
@@ -9,3 +9,23 @@ class InvalidSlimName(Exception):
             f"{', '.join([slim.get('name') for slim in valid_slim_list])}."
         )
         super().__init__(self.message)
+
+
+class CellTypeNotFoundError(Exception):
+    def __init__(self, cell_type: str, cell_type_list: List[str]):
+        self.message = (
+            f"{cell_type} not found in the annotations."
+            f"Please use cell types from: "
+            f"{', '.join(cell_type_list)}."
+        )
+        super().__init__(self.message)
+
+
+class SubclassWarning(Exception):
+    def __init__(self, relation: List[List[str]]):
+        joined_relations = ", ".join(["-".join(rel) for rel in relation])
+        self.message = (
+            f"The following cell type terms are related with subClassOf relation. "
+            f"{joined_relations}."
+        )
+        super().__init__(self.message)
