b393ff48305d92af78e2b12c568308994f2ba938
Ugur Bayindir <ubyndr@gmail.com>
2023-07-21 11:01:54 +0100

    Added filter_anndata_with_enriched_cell_type and annotate_anndata_with_cell_type methods

--- pandasaurus_cxg/anndata_enricher.py
+++ pandasaurus_cxg/anndata_enricher.py
@@ -1,12 +1,17 @@
+import itertools
 from typing import List, Optional
 
-from anndata import AnnData
 import pandas as pd
+from anndata import AnnData
 from pandasaurus.query import Query
 from pandasaurus.slim_manager import SlimManager
 
 from pandasaurus_cxg.anndata_loader import AnndataLoader
-from pandasaurus_cxg.utils.exceptions import InvalidSlimName
+from pandasaurus_cxg.utils.exceptions import (
+    CellTypeNotFoundError,
+    InvalidSlimName,
+    SubclassWarning,
+)
 
 
 class AnndataEnricher:
@@ -36,6 +41,12 @@ class AnndataEnricher:
         # TODO Do we need to keep whole anndata? Would it be enough to keep the obs only?
         # file_path: The path to the file containing the anndata object.
         self._anndata = anndata
+        self.cell_type_dict = (
+            self._anndata.obs[["cell_type_ontology_term_id", "cell_type"]]
+            .drop_duplicates()
+            .set_index("cell_type_ontology_term_id")["cell_type"]
+            .to_dict()
+        )
         self.__seed_list = self._anndata.obs[cell_type_field].unique().tolist()
         self.__enricher = Query(self.__seed_list)
         self.__context_list = (
@@ -114,17 +125,78 @@ class AnndataEnricher:
         )
         return self.enriched_df
 
-    def filter_anndata_by_enriched_cell_type(self) -> pd.DataFrame:
+    def filter_anndata_with_enriched_cell_type(self, cell_type: str) -> pd.DataFrame:
         """Filter the original anndata object based on enriched cell types.
 
+        Args:
+            cell_type: CURIE of the cell type for filtering.
+
         Returns:
-            The filtered observations from the original anndata object.
+            pd.DataFrame: A DataFrame containing the filtered observations from the original
+                anndata object.
+
+        Raises:
+            CellTypeNotFoundError: If the provided cell_type is not found in the enriched cell types.
+
         """
-        # TODO 's' amd 'o' part should be revised
-        cell_type_list = set(self.enriched_df["s"].tolist() + self.enriched_df["o"].tolist())
+        # TODO Add empty dataframe exception
+        cell_type_dict = self.create_cell_type_dict()
+        if cell_type not in cell_type_dict:
+            raise CellTypeNotFoundError(cell_type, cell_type_dict.keys())
+
+        cell_type_group = self.enriched_df[self.enriched_df["o"] == cell_type]["s"].tolist()
+        cell_type_group.append(cell_type)
+
         return self._anndata.obs[
-            self._anndata.obs["cell_type_ontology_term_id"].isin(cell_type_list)
+            self._anndata.obs["cell_type_ontology_term_id"].isin(cell_type_group)
         ]
+
+    def annotate_anndata_with_cell_type(
+        self, cell_type_list: List[str], field_name: str, field_value: str
+    ) -> pd.DataFrame:
+        """Annotates the AnnData object with cell type information.
+
+        This method annotates the cells in the AnnData object with specific cell type
+        information based on the provided `cell_type_list`. It sets the value of the specified
+        `field_name` to the given `field_value` for the cells whose 'cell_type_ontology_term_id'
+        matches any of the values in `cell_type_list`.
+
+        Args:
+            cell_type_list (List[str]): A list of cell type ontology term IDs to be used for
+                cell type annotation.
+            field_name (str): The name of the field/column in the AnnData object where the
+                cell type information will be stored.
+            field_value (str): The value to be assigned to cells with matching cell type
+                ontology term IDs in the specified `field_name`.
+
+        Returns:
+            pd.DataFrame: A DataFrame containing the updated observations from the original anndata
+                object.
+
+        Raises:
+            CellTypeNotFoundError: If any cell type in `cell_type_list` is not found in the
+                available cell types in the dataset.
+            SubclassWarning: If the provided `cell_type_list` contains any relationships where
+                one cell type is a subclass of another, indicating a potential issue with the
+                provided annotations.
+        """
+        # TODO Add empty dataframe exception
+        cell_type_dict = self.create_cell_type_dict()
+        # Check if any cell_type in cell_type_list is not in cell_type_dict
+        missing_cell_types = set(cell_type_list) - set(cell_type_dict.keys())
+        if missing_cell_types:
+            raise CellTypeNotFoundError(missing_cell_types, list(cell_type_dict.keys()))
+
+        # preprocess
+        subclass_relation = self.check_subclass_relationships(cell_type_list)
+        if subclass_relation:
+            raise SubclassWarning(subclass_relation)
+
+        # annotation phase
+        self._anndata.obs[field_name] = ""
+        condition = self._anndata.obs["cell_type_ontology_term_id"].isin(cell_type_list)
+        self._anndata.obs.loc[condition, field_name] = field_value
+        return self._anndata.obs[self._anndata.obs[field_name] == field_value]
 
     def set_enricher_property_list(self, property_list: List[str]):
         """Set the property list for the enricher.
@@ -152,3 +224,35 @@ class AnndataEnricher:
 
     def get_seed_list(self):
         return self.__seed_list
+
+    def create_cell_type_dict(self):
+        # TODO Add empty dataframe exception
+        temp_df = pd.concat(
+            [
+                self.enriched_df[["s", "s_label"]],
+                self.enriched_df[["o", "o_label"]].rename(columns={"o": "s", "o_label": "s_label"}),
+            ],
+            axis=0,
+            ignore_index=True,
+        )
+        cell_type_dict = temp_df.drop_duplicates().set_index("s")["s_label"].to_dict()
+        return cell_type_dict
+
+    def check_subclass_relationships(self, cell_type_list: List[str]):
+        # TODO Add empty dataframe exception
+        subclass_relation = []
+        for s, o in itertools.combinations(cell_type_list, 2):
+            if not self.enriched_df[
+                (self.enriched_df["s"] == s)
+                & (self.enriched_df["p"] == "rdfs:subClassOf")
+                & (self.enriched_df["o"] == o)
+            ].empty:
+                subclass_relation.append([s, o])
+
+            if not self.enriched_df[
+                (self.enriched_df["s"] == o)
+                & (self.enriched_df["p"] == "rdfs:subClassOf")
+                & (self.enriched_df["o"] == s)
+            ].empty:
+                subclass_relation.append([o, s])
+        return subclass_relation
