fd90191eee0c0ed13f1ae922cac2fffb6322fa8d
Ugur Bayindir <ubyndr@gmail.com>
2023-07-31 14:44:12 +0100

    Format changes in co_annotation_report

--- pandasaurus_cxg/anndata_analyzer.py
+++ pandasaurus_cxg/anndata_analyzer.py
@@ -65,7 +65,9 @@ class AnndataAnalyzer:
                     and field_name_1 in self._anndata.obs.columns
                     and field_name_2 in self._anndata.obs.columns
                 ):
-                    co_oc = self._filter_data_and_drop_duplicates(field_name_1, field_name_2, disease)
+                    co_oc = self._filter_data_and_drop_duplicates(
+                        field_name_1, field_name_2, disease
+                    )
 
                     if enrich:
                         co_oc = self._enrich_co_annotation(co_oc, field_name_1, field_name_2)
