6ae46123855db39d7f9e1851833b388e313708e9
Ismail Ugur Bayindir <ugur@ebi.ac.uk>
2023-08-08 15:24:52 +0100

    Add function to filter anndata matrix by cell type (#25)
    * Clean up
    
    * Added an example to filter AnnData matrix by Cell Type
    
    * Added Discussion cells
    
    * Added Discussion cells
    
    * Added CellTypeNotFoundError and SubclassWarning
    
    * Refactored imports
    
    * Added filter_anndata_with_enriched_cell_type and annotate_anndata_with_cell_type methods
    
    * Updated anndata_analyzer.py
    
    * Updated anndata_analyzer.py
    
    * Refactored create_cell_type_dict method
    
    * Removed cell_type_dict from init

--- pandasaurus_cxg/anndata_analyzer.py
+++ pandasaurus_cxg/anndata_analyzer.py
@@ -1,6 +1,6 @@
-from enum import Enum
 import itertools
 import os
+from enum import Enum
 from typing import List, Optional
 
 import pandas as pd
@@ -8,7 +8,6 @@ import pandas as pd
 from pandasaurus_cxg.anndata_enricher import AnndataEnricher
 from pandasaurus_cxg.anndata_loader import AnndataLoader
 from pandasaurus_cxg.schema.schema_loader import read_json_file
-
 
 # Check if the DEBUG environment variable is set
 debug_mode = os.getenv("DEBUG")
@@ -50,7 +49,7 @@ class AnndataAnalyzer:
                 given disease. If provided, only the rows matching the specified disease will be
                 included in the filtering process. Defaults to None if no disease filtering is
                 desired.
-            enrich: Flag to either enable or disable enrichment in co_annotation report.
+            enrich (bool): Flag to either enable or disable enrichment in co_annotation report.
                 Defaults to False.
 
         Returns:
@@ -59,20 +58,22 @@ class AnndataAnalyzer:
         """
         free_text_cell_type = [key for key, value in self._schema.items() if value]
         temp_result = []
-        cell_type_combinations = list(itertools.combinations(free_text_cell_type, 2))
-        for combination in cell_type_combinations:
-            field_name_1 = combination[1]
-            field_name_2 = combination[0]
-            if (field_name_1 in self._anndata.obs.columns
-                and field_name_2 in self._anndata.obs.columns
-            ):
-                co_oc = self._filter_data_and_remove_duplicates(field_name_1, field_name_2, disease)
+        for field_name_2 in free_text_cell_type:
+            for field_name_1 in free_text_cell_type:
+                if (
+                        field_name_1 != field_name_2
+                        and field_name_1 in self._anndata.obs.columns
+                        and field_name_2 in self._anndata.obs.columns
+                ):
+                    co_oc = self._filter_data_and_drop_duplicates(
+                        field_name_1, field_name_2, disease
+                    )
 
-                if enrich:
-                    co_oc = self._enrich_co_annotation(co_oc, field_name_1, field_name_2)
+                    if enrich:
+                        co_oc = self._enrich_co_annotation(co_oc, field_name_1, field_name_2)
 
-                AnndataAnalyzer._assign_predicate_column(co_oc, field_name_1, field_name_2)
-                temp_result.extend(co_oc.to_dict(orient="records"))
+                    AnndataAnalyzer._assign_predicate_column(co_oc, field_name_1, field_name_2)
+                    temp_result.extend(co_oc.to_dict(orient="records"))
 
         result = [
             [item for sublist in [[k, v] for k, v in record.items()] for item in sublist]
@@ -83,19 +84,6 @@ class AnndataAnalyzer:
             [inner_list[:2] + inner_list[5:6] + inner_list[2:4] for inner_list in unique_result],
             columns=["field_name1", "value1", "predicate", "field_name2", "value2"],
         )
-
-    def _filter_data_and_remove_duplicates(self, field_name_1, field_name_2, disease):
-        # Filter the data based on the disease condition
-        co_oc = (
-            self._anndata.obs[
-                (self._anndata.obs["disease_ontology_term_id"].str.lower() == disease.lower())
-            ][[field_name_1, field_name_2]]
-            if disease
-            else self._anndata.obs[[field_name_1, field_name_2]]
-        )
-        # Drop duplicates
-        co_oc = co_oc.drop_duplicates().reset_index(drop=True)
-        return co_oc
 
     def enriched_co_annotation_report(self, disease: Optional[str] = None):
         """
@@ -116,18 +104,18 @@ class AnndataAnalyzer:
             pd.DataFrame: The co-annotation report.
 
         """
-        return self.co_annotation_report(self, disease, True)
+        return self.co_annotation_report(disease, True)
 
     def _enrich_co_annotation(self, co_oc, field_name_1, field_name_2):
         enricher = AnndataEnricher(self._anndata)
         simple = enricher.simple_enrichment()
-        df = simple[simple["o"].isin(enricher.get_seed_list())][
-            ["s_label", "o_label"]
-        ].rename(columns={"s_label": field_name_1, "o_label": field_name_2})
+        df = simple[simple["o"].isin(enricher.get_seed_list())][["s_label", "o_label"]].rename(
+            columns={"s_label": field_name_1, "o_label": field_name_2}
+        )
         co_oc = pd.concat([co_oc, df], axis=0).reset_index(drop=True)
         return co_oc
 
-    def _filter_data_and_remove_duplicates(self, field_name_1, field_name_2, disease):
+    def _filter_data_and_drop_duplicates(self, field_name_1, field_name_2, disease):
         # Filter the data based on the disease condition
         co_oc = (
             self._anndata.obs[
