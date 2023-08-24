efa31a2456b89cad928f1e49c4f528f7560ecbbc
Ugur Bayindir <ubyndr@gmail.com>
2023-08-24 18:30:10 +0100

    Format changes

--- pandasaurus_cxg/graph_generator/graph_predicates.py
+++ pandasaurus_cxg/graph_generator/graph_predicates.py
@@ -1,14 +1,5 @@
-CONSIST_OF = {
-    "iri": "http://purl.obolibrary.org/obo/RO_0002473",
-    "label": "composed_primarily_of"
-}
+CONSIST_OF = {"iri": "http://purl.obolibrary.org/obo/RO_0002473", "label": "composed_primarily_of"}
 
-SUBCLUSTER_OF = {
-    "iri": "http://purl.obolibrary.org/obo/RO_0015003",
-    "label": "subcluster of"
-}
+SUBCLUSTER_OF = {"iri": "http://purl.obolibrary.org/obo/RO_0015003", "label": "subcluster of"}
 
-CLUSTER = {
-    "iri": "http://purl.obolibrary.org/obo/PCL_0010001",
-    "label": "cell cluster"
-}
+CLUSTER = {"iri": "http://purl.obolibrary.org/obo/PCL_0010001", "label": "cell cluster"}
