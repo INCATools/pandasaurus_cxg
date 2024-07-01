848ca96424c9d064b1b9e943d09c45cf9498df65
Ismail Ugur Bayindir <ubyndr@gmail.com>
2024-07-01 19:05:19 +0300

    Refactored CURIE generation logic for annotated_target (#76)
    * Refactored annotated_target curie generation
    
    * Updated version to 0.1.18

---
+++ pandasaurus_cxg/graph_generator/graph_namespaces.py
@@ -0,0 +1,8 @@
+prefixes = {
+    "CL": "http://purl.obolibrary.org/obo/CL_",
+    "UBERON": "http://purl.obolibrary.org/obo/UBERON_",
+    "PATO": "http://purl.obolibrary.org/obo/PATO_",
+    "MONDO": "http://purl.obolibrary.org/obo/MONDO_",
+    "HsapDv": "http://purl.obolibrary.org/obo/HsapDv_",
+    "NCBITaxon": "http://purl.obolibrary.org/obo/NCBITaxon_",
+}
